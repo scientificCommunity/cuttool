e15163893cb3e2695892b1bc90de8fb01239f33b
Krivers <305809299@qq.com>
2026-03-26 13:14:04 +0800

    init cutout tool

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 如果仓库名是 用户名.github.io，就改成 '/'
+  base: '/cuttool/', // 如果仓库名是 用户名.github.io，就改成 '/'
 })
